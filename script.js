dc5293aa6c83ffeae039690aaf1e9cd9d60c2f25
JtheFox <60980739+JtheFox@users.noreply.github.com>
2022-05-27 13:18:41 -0400

    Added conditions for past/present/future styling

--- script.js
+++ script.js
@@ -11,12 +11,18 @@ $(function() {
 });
 
 function createTimeBlock(startTime) {
-    const time = startTime > 11 ? `${startTime - 12 || 12}PM` : `${startTime}AM`
+    const currTime = parseInt(moment().format('H'));
+    const time = startTime > 11 ? `${startTime - 12 || 12}PM` : `${startTime}AM`;
+    let relTime;
+    console.log(startTime, currTime)
+    if (startTime < currTime) relTime = 'past';
+    else if (startTime === currTime) relTime = 'present';
+    else relTime = 'future';
 
     const timeBlock = $('<div></div>').addClass('time-block row');
     const hour = $('<p></p>').addClass('hour').text(time);
     timeBlock.append(hour);
-    const textarea = $('<textarea rows="5"></textarea>').addClass(`description present`) // add past/present/future class conditional
+    const textarea = $('<textarea rows="5"></textarea>').addClass(`description ${relTime}`)
     timeBlock.append(textarea);
     const saveBtn = $('<button></button>').addClass('saveBtn').text('💾');
     timeBlock.append(saveBtn);
